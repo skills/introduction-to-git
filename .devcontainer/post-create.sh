dba8cafde04e777121672e4c2ae13818bd2ac09a
Christopher W. Blake <chriswblake@github.com>
2025-11-21 00:20:23 +0000

    start post-create script with git hooks

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,2 +1,6 @@
-# Add Git hooks to monitor exercise progress
-git config --global core.hooksPath /workspaces/playground/.devcontainer/.githooks
+# Install Git hooks to monitor exercise progress
+cp -r /workspaces/${RepositoryName}/.devcontainer/.githooks /home/vscode/.vscode-remote/data/Machine/.githooks
+git config --global core.hooksPath /home/vscode/.vscode-remote/data/Machine/.githooks
+
+# Only use paging if the results are longer than one screen
+git config --global core.pager "less -FX"
