148fc23b0a6a8ced308407fcf969996e0ff579df
Christopher W. Blake <chriswblake@github.com>
2025-11-21 00:56:25 +0000

    modify script to remove original repo

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -4,3 +4,7 @@ git config --global core.hooksPath /home/vscode/.vscode-remote/data/Machine/.git
 
 # Only use paging if the results are longer than one screen
 git config --global core.pager "less -FX"
+
+# Remove original repository, to keep the workspace clean
+sleep 5
+rm -R -f /workspaces/${RepositoryName}
