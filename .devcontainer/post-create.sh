fd318e559bcd1098bfd3c1c7e4548ea17510ea73
Christopher W. Blake <chriswblake@github.com>
2025-11-22 00:25:18 +0000

    switch to hiding the repo, instead of deleting it.

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -6,7 +6,3 @@ git config --global core.pager "less -FX"
 
 # Install exercise monitor - Do this last to avoid accidental triggers during startup.
 /workspaces/${RepositoryName}/.devcontainer/exercise-monitor/install.sh
-
-# Remove original repository, to keep the workspace clean
-sleep 5
-rm -R -f /workspaces/${RepositoryName}
