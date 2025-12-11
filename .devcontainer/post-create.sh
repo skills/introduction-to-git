c8fc54aa77d9f3d57283f451f164bb1f5f74749b
Christopher W. Blake <chriswblake@github.com>
2025-12-11 19:30:23 +0000

    feat: automatically prepare sample project

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
 # Only use paging if the results are longer than one screen.
-# Do this before installing the execise monitor
+# Do this before installing the exercise monitor
 git config --global core.pager "less -FX"
 
 # Install exercise monitor - Do this last to avoid accidental triggers during startup.
 /workspaces/${RepositoryName}/.devcontainer/exercise-monitor/install.sh
+
+# Create sample project if it doesn't exist
+REPO_DIR="/workspaces/stack-overflown"
+if [ ! -d "$REPO_DIR" ]; then
+  #  Create project folder
+  mkdir -p "$REPO_DIR"
+
+  # Copy sample source code
+  cp -r /workspaces/${RepositoryName}/src/. $REPO_DIR/src/
+fi
