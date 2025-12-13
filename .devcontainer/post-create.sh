36ff92b1cf00fc923105aec670bddea8209054db
Christopher W. Blake <chriswblake@github.com>
2025-12-13 22:25:01 +0000

    remove .git folder in exercise folder to prevent showing in source control

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -7,6 +7,9 @@ git config --global core.pager "less -FX"
 # Install exercise monitor - Do this last to avoid accidental triggers during startup.
 /workspaces/${RepositoryName}/.devcontainer/exercise-monitor/install.sh
 
+# Delete the .git folder in the exercise repo to prevent from showing in version control
+rm -R -f /workspaces/${RepositoryName}/.git
+
 # Create sample project if it doesn't exist
 REPO_DIR="/workspaces/stack-overflown"
 if [ ! -d "$REPO_DIR" ]; then
